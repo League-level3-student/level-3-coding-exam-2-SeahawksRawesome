a4a76f3ef439900ef2e76e08fb3e311a70836aaa
SeahawksRawesome <league@WTS22.attlocal.net>
2018-07-01 16:30:24 -0700

    YAY, IT IS DHONE!
    SKRRT

--- src/test/HashMapCalculator.java
+++ src/test/HashMapCalculator.java
@@ -3,16 +3,32 @@ package test;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * PART B
+ * 
+ * Write a method that takes in an ArrayList of Integers and a HashMap that
+ * matches and Integer key to a String Value. The method should return an
+ * ArrayList of all the Values from the HashMap that have matching keys to the
+ * passed in ArrayList.
+ * 
+ */
+
 public class HashMapCalculator {
 
 	public ArrayList<String> commonKeyValues(ArrayList<Integer> lst, HashMap<Integer, String> hm) {
+		ArrayList<String> st = new ArrayList<String>();
 		int hashChecker = 0;
 		for (int i = 0; i < lst.size(); i++) {
 			hashChecker = lst.get(i);
-			hm.get(hashChecker);
 
+			if (hm.get(hashChecker) != null) {
+				String storeInArray = hm.get(hashChecker);
+				st.add(storeInArray);
+			} else {
+
+			}
 		}
-		return null;
+		return st;
 	}
 
 }
